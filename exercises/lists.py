f72128c6ebb62635e1034eaf384ccce96e3d67b3
Vladimir-Fedorov <neroazzurri@mail.ru>
2022-11-12 20:05:35 +0400

    Implemented the method replace

--- exercises/lists.py
+++ exercises/lists.py
@@ -7,7 +7,21 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        max_elem = 0
+        result = []
+
+        for elem in input_list:
+            if elem > max_elem:
+                max_elem = elem
+
+        for elem in input_list:
+            if elem > 0:
+                result.append(max_elem)
+            else:
+                result.append(elem)
+
+        return result
+
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
@@ -18,4 +32,19 @@ class ListExercise:
         :param query: Искомый элемент
         :return: Номер элемента
         """
+        # if len(input_list) == 0:
+        #   return -1
+        # if len(input_list) == 1 and input_list[0] != query:
+        #   return -1
+        # if input_list[len(input_list) // 2] == query:
+        #   return
         pass
+
+
+print(ListExercise.search([1], 900))
+# ListExercise.search([1], 900) == -1
+# ListExercise.search([1], 1) == 0
+# ListExercise.search([], 900) == -1
+# ListExercise.search([1, 4, 5, 7, 8, 9], 9) == 5
+# ListExercise.search([1, 4, 5, 7, 8, 9], 1) == 0
+# ListExercise.search([1, 4, 5, 7, 8, 9], 6) == -1
