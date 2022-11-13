af1ce91a0e46ab1cf7eda860f36bad688867fd2c
Vladimir-Fedorov <neroazzurri@mail.ru>
2022-11-13 17:29:55 +0400

    Implemented methods "replace" and "search"

--- exercises/lists.py
+++ exercises/lists.py
@@ -8,12 +8,11 @@ class ListExercise:
         :return: Список с замененными элементами
         """
         max_elem = 0
-        result = []
-
         for elem in input_list:
             if elem > max_elem:
                 max_elem = elem
 
+        result = []
         for elem in input_list:
             if elem > 0:
                 result.append(max_elem)
@@ -21,7 +20,6 @@ class ListExercise:
                 result.append(elem)
 
         return result
-
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
@@ -32,19 +30,29 @@ class ListExercise:
         :param query: Искомый элемент
         :return: Номер элемента
         """
-        # if len(input_list) == 0:
-        #   return -1
-        # if len(input_list) == 1 and input_list[0] != query:
-        #   return -1
-        # if input_list[len(input_list) // 2] == query:
-        #   return
-        pass
+        inside_input_list = input_list
+        inside_query = query
 
+        def custom_binary_search(inside_input_list: list[int], inside_query: int,
+                                 min: int = 0, max: int = len(inside_input_list) - 1) -> int:
+            """
+            Функция возвращает индекс элемента
+            :param inside_input_list: Исходный список
+            :param inside_query: Искомый элемент
+            :param min: Индекс элемента с минимальным значением
+            :param max: Индекс элемента с максимальным значением
+            :return: Номер элемента
+            """
+            if min > max or inside_query > inside_input_list[max] or inside_query < inside_input_list[min]:
+                return -1
 
-print(ListExercise.search([1], 900))
-# ListExercise.search([1], 900) == -1
-# ListExercise.search([1], 1) == 0
-# ListExercise.search([], 900) == -1
-# ListExercise.search([1, 4, 5, 7, 8, 9], 9) == 5
-# ListExercise.search([1, 4, 5, 7, 8, 9], 1) == 0
-# ListExercise.search([1, 4, 5, 7, 8, 9], 6) == -1
+            mid = (min + max) // 2
+            if inside_input_list[mid] == inside_query:
+                return mid
+
+            if inside_input_list[mid] > inside_query:
+                return custom_binary_search(inside_input_list, inside_query, min, mid - 1)
+            else:
+                return custom_binary_search(inside_input_list, inside_query, mid + 1, max)
+
+        return custom_binary_search(inside_input_list, inside_query)
